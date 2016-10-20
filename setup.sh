455210a7453043d33e160dcc5aa8288c8a1eb03b
lilylabo <blue@lily.la>
2016-10-20 19:35:50 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,8 +11,20 @@ sudo defaults write /Library/Preferences/com.apple.security GKAutoRearm -bool NO
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
+brew install rcmdnk/file/brew-file
+# add this code to .bash_profile
+#if [ -f $(brew --prefix)/etc/brew-wrap ];then
+#source $(brew --prefix)/etc/brew-wrap
+#fi
+
 brew tap caskroom/cask
 brew tap caskroom/versions
+
+# for the Mac App Store
+brew install mas
+
+# for Sierra
+#brew unlink php70
 
 # install from Brewfile
 brew file set_repo
@@ -21,3 +33,13 @@ brew file install
 
 brew cleanup
 brew cask cleanup
+
+
+# OS settings
+defaults write com.apple.finder AppleShowAllFiles -bool yes
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool yes
+defaults write com.apple.finder QLEnableTextSelection -bool yes
+killall Finder
+
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+defaults write -g NSNavPanelExpandedStateForSaveMode -bool yes
